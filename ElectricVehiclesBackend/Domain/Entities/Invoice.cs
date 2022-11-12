45d61b73ea917ef5192b0cf029f7139526c257c1
MihaiFlorin01 <mihai@DESKTOP-2IC34IH>
2022-11-12 17:24:59 +0200

    First commit and push

---
+++ ElectricVehiclesBackend/Domain/Entities/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public decimal GrossAmount { get; set; }
+        public decimal VAT { get; set; }
+        public decimal NetAmount { get; set; }
+        public bool Paid { get; set; }
+    }
+}
---
+++ ElectricVehiclesBackend/Domain/Entities/Rentals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Rentals
+    {
+        public int Id { get; set; }
+        public int BikeId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public int InvoiceId { get; set; }
+    }
+}
