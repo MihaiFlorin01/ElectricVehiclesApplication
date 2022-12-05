10e8e05b7266869e66a1c489c87b745a2697c52a
MihaiFlorin01 <mihai@DESKTOP-2IC34IH>
2022-12-05 20:07:16 +0200

    Added some changes

--- ElectricVehiclesFrontend/src/app/app.module.ts
+++ ElectricVehiclesFrontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeComponent } from './components/bike/bike.component';
+import { UserComponent } from './components/user/user.component';
+import { BikeTypeComponent } from './components/bike-type/bike-type.component';
+import { LoginComponent } from './login/login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikeComponent,
+    UserComponent,
+    BikeTypeComponent,
+    LoginComponent,
   ],
   imports: [
+    AppRoutingModule,
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
