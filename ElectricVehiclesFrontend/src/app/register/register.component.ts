3b80f83953edaccda68c2a45e7fcf5d22eff4993
MihaiFlorin01 <mihai@DESKTOP-2IC34IH>
2022-12-11 23:13:29 +0200

    Added some features

--- ElectricVehiclesFrontend/src/app/register/register.component.ts
+++ ElectricVehiclesFrontend/src/app/register/register.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
+  hide = true;
+  registerForm!: FormGroup;
+  emailRegex = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(this.emailRegex)]],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
 }
