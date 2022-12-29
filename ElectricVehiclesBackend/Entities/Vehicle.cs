246d4d02b25839bf6044702b3a0fb7c120874c09
MihaiFlorin01 <mihai@DESKTOP-2IC34IH>
2022-12-29 04:15:02 +0200

    Added last changes

--- ElectricVehiclesBackend/Entities/Vehicle.cs
+++ ElectricVehiclesBackend/Entities/Vehicle.cs
@@ -1,11 +1,13 @@
 ﻿using Abstractions;
+using Utils.Enums;
 
 namespace Entities
 {
     public class Vehicle : BaseEntityModel
     {
-        public string? Type { get; set; }
+        public VehicleTypeRegistered Type { get; set; }
         public DateTimeOffset RegisterDate { get; set; }
+        public VehicleType? VehicleType { get; set; }
         public ICollection<Rental>? Rentals { get; set; }
     }
 }
---
+++ ElectricVehiclesBackend/Utils/Enums/VehicleTypeRegistered.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils.Enums
+{
+    public enum VehicleTypeRegistered
+    {
+        ClassicScooter = 1,
+        ElectricScooter = 2,
+        ClassicBike = 3,
+        ElectricBike = 4,
+    }
+}
